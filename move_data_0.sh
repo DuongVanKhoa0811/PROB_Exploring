133e3da35a2aff517e3453ea2c6d8d0e73da5d37
khoa <duongvankhoadt@gmail.com>
2025-10-26 12:44:43 +0800

    Nothing

---
+++ move_data_0.sh
@@ -0,0 +1,2 @@
+# scp -r khoadv@172.31.124.78:/home/khoa/projects/OOD_OD/PROB_Exploring/data/OWOD/JPEGImages /home/khoadv/projects/OOD_OD/PROB_Exploring/data/OWOD
+# scp -r khoadv@172.31.124.78:/home/khoa/projects/OOD_OD/PROB_Exploring/data/OWOD/Annotations /home/khoadv/projects/OOD_OD/PROB_Exploring/data/OWOD
