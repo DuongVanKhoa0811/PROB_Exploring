18bf8a80aec72a0743ca31f50c6ff4be710e83d7
DuongVanKhoa0811 <dvkhoa19@apcs.fitus.edu.vn>
2025-10-26 13:46:07 +0800

    Nothing

--- move_data_0.sh
+++
@@ -1,2 +0,0 @@
-# scp -r khoadv@172.31.124.78:/home/khoa/projects/OOD_OD/PROB_Exploring/data/OWOD/JPEGImages /home/khoadv/projects/OOD_OD/PROB_Exploring/data/OWOD
-# scp -r khoadv@172.31.124.78:/home/khoa/projects/OOD_OD/PROB_Exploring/data/OWOD/Annotations /home/khoadv/projects/OOD_OD/PROB_Exploring/data/OWOD
