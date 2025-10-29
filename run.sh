cc78669ac2fdc88c5acbc1c8ae88c044e6718bc8
DuongVanKhoa0811 <dvkhoa19@apcs.fitus.edu.vn>
2025-10-29 23:42:07 +0800

    Nothing

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 # #!/bin/bash
-export CUDA_VISIBLE_DEVICES=1
-GPUS_PER_NODE=1 ./tools/run_dist_launch.sh 1 configs/S_OWOD_BENCHMARK.sh
+# export CUDA_VISIBLE_DEVICES=0,1
+
+GPUS_PER_NODE=2 ./tools/run_dist_launch.sh 2 configs/M_OWOD_BENCHMARK.sh
