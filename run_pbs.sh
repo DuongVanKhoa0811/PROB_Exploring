69d67f056863ec1c17c01f6ec5a905603980095e
DuongVanKhoa0811 <dvkhoa19@apcs.fitus.edu.vn>
2025-11-14 21:35:27 +0800

    Update run_pbs.sh

--- run_pbs.sh
+++ run_pbs.sh
@@ -3,24 +3,11 @@
 #PBS -q gold
 #PBS -l select=1:ncpus=8:ngpus=2
 #PBS -l walltime=99:00:00
-#PBS -o /home/users/vankhoa_duong/projects/OOD_OD/PROB_Exploring/trash/output.log
-#PBS -e /home/users/vankhoa_duong/projects/OOD_OD/PROB_Exploring/trash/err.log
+#PBS -j oe
+#PBS -o /home/users/vankhoa_duong/projects/OOD_OD/PROB_Exploring/logs/output.log
 
 cd /home/users/vankhoa_duong/projects/OOD_OD/PROB_Exploring
-source ~/.bashrc  # or source /path/to/conda.sh
-
-# Activate conda environment
+source ~/.bashrc
 conda activate prob
 
 bash run.sh > logs/logs_PROB_SOWODB_Vall.txt 2>&1
-
-# # Set CUDA environment variables if needed
-# export CUDA_HOME=$CONDA_PREFIX
-# export PATH=$CUDA_HOME/bin:$PATH
-# export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
-
-# # Navigate to ops directory and run make.sh
-# sh make.sh > a.txt
-
-# Optionally test
-# python test.py > b.txt 2>&1
