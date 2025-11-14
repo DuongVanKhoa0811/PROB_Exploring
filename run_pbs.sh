755a320817eeb70fbe9e351bb0188b7f5508e474
DuongVanKhoa0811 <dvkhoa19@apcs.fitus.edu.vn>
2025-11-15 01:08:53 +0800

    Update run_pbs.sh

--- run_pbs.sh
+++ run_pbs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N PROB
 #PBS -q gold
-#PBS -l select=1:ncpus=8:ngpus=2
+#PBS -l select=1:ncpus=8:ngpus=2:mem=64gb
 #PBS -l walltime=99:00:00
 #PBS -j oe
 #PBS -o /home/users/vankhoa_duong/projects/OOD_OD/PROB_Exploring/logs/output.log
