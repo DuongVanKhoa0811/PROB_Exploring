4dbf5aececd55f4e76757b4af9f725e7605255b3
DuongVanKhoa0811 <dvkhoa19@apcs.fitus.edu.vn>
2025-11-15 12:40:09 +0800

    exp_obj_train_only: Test #2 - Set --obj_loss_coef to zero

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # export CUDA_VISIBLE_DEVICES=0,1
 
-GPUS_PER_NODE=2 ./tools/run_dist_launch.sh 2 configs/M_OWOD_BENCHMARK.sh
+GPUS_PER_NODE=2 ./tools/run_dist_launch.sh 2 configs/M_OWOD_BENCHMARK_freeze_obj.sh
