7c639d6f321c46aba13a5e56bb2085c97b557a69
DuongVanKhoa0811 <dvkhoa19@apcs.fitus.edu.vn>
2025-11-17 11:49:33 +0800

    exp_obj_train_only: Test #4 - Eval

--- run_eval.sh
+++ run_eval.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
-export CUDA_VISIBLE_DEVICES=1
+# export CUDA_VISIBLE_DEVICES=1
 
-GPUS_PER_NODE=1 ./tools/run_dist_launch_eval.sh 1 configs/EVAL_M_OWOD_BENCHMARK_V1.sh > ./logs/logs_PROB_MOWODB_EVAL_V1.txt 2>&1
+GPUS_PER_NODE=2 ./tools/run_dist_launch_eval.sh 2 configs/EVAL_M_OWOD_BENCHMARK_V7.sh > ./logs/logs_PROB_MOWODB_EVAL_V7.txt 2>&1
