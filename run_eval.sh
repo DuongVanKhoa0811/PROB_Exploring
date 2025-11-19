a75c6c7cb42882c3e7eb689138d1b03c230a1b91
DuongVanKhoa0811 <dvkhoa19@apcs.fitus.edu.vn>
2025-11-19 16:23:01 +0800

    Eval for MOWODB Vall

--- run_eval.sh
+++ run_eval.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=2
 
-GPUS_PER_NODE=1 ./tools/run_dist_launch_eval.sh 1 configs/EVAL_M_OWOD_BENCHMARK_V1.sh > ./logs/logs_PROB_MOWODB_EVAL_V1.txt 2>&1
+GPUS_PER_NODE=2 ./tools/run_dist_launch_eval.sh 2 configs/EVAL_M_OWOD_BENCHMARK.sh > ./logs/logs_PROB_MOWODB_EVAL_Vall.txt 2>&1
