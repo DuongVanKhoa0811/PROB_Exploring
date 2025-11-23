86dd7edba41f71581e330dcf9c1afd05560b6b46
DuongVanKhoa0811 <dvkhoa19@apcs.fitus.edu.vn>
2025-11-23 09:46:06 +0800

    exp_obj_train_only: Test #4 - Update run_pbs.sh

--- run_pbs.sh
+++ run_pbs.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-#PBS -N PROB_MOWODB_V3
+#PBS -N PROB_M_V11
 #PBS -q gold
-#PBS -l select=1:ncpus=8:ngpus=2
-#PBS -l walltime=36:00:00
+#PBS -l select=1:ncpus=8:ngpus=8:mem=64gb
+#PBS -l walltime=99:00:00
 #PBS -j oe
-#PBS -o ${PBS_O_WORKDIR}/trash/logs_PROB_MOWODB_V3.log
+#PBS -o /home/users/vankhoa_duong/projects/OOD_OD/PROB_Exploring/logs/output.log
 
-bash run.sh > ./logs/logs_PROB_MOWODB_V3.txt 2>&1
+cd /home/users/vankhoa_duong/projects/OOD_OD/PROB_Exploring
+source ~/.bashrc
+conda activate prob
+
+bash run.sh
