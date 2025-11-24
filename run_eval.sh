2a5b678f6ec3535fcc54fa24d48b58eb5ada226d
DuongVanKhoa0811 <dvkhoa19@apcs.fitus.edu.vn>
2025-11-24 09:59:55 +0800

    exp_obj_train_only: Test #5 - Eval V12_1, V14_1

--- run_eval.sh
+++ run_eval.sh
@@ -2,6 +2,5 @@
 set -ex
 # export CUDA_VISIBLE_DEVICES=1
 
-GPUS_PER_NODE=2 ./tools/run_dist_launch_eval.sh 2 configs/EVAL_M_OWOD_BENCHMARK_train_obj_0.sh > ./logs/logs_PROB_MOWODB_EVAL_V8_0.txt 2>&1
-GPUS_PER_NODE=2 ./tools/run_dist_launch_eval.sh 2 configs/EVAL_M_OWOD_BENCHMARK_train_obj_1.sh > ./logs/logs_PROB_MOWODB_EVAL_V8_1.txt 2>&1
-GPUS_PER_NODE=2 ./tools/run_dist_launch_eval.sh 2 configs/EVAL_M_OWOD_BENCHMARK_train_obj_2.sh > ./logs/logs_PROB_MOWODB_EVAL_V8_2.txt 2>&1
+GPUS_PER_NODE=2 ./tools/run_dist_launch_eval.sh 2 configs/EVAL_M_OWOD_BENCHMARK_train_obj_1_V12.sh > ./logs/logs_PROB_MOWODB_EVAL_V12_1.txt 2>&1
+GPUS_PER_NODE=2 ./tools/run_dist_launch_eval.sh 2 configs/EVAL_M_OWOD_BENCHMARK_train_obj_1_V14.sh > ./logs/logs_PROB_MOWODB_EVAL_V14_1.txt 2>&1
