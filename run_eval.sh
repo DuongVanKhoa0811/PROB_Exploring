8a0c13be7a7c7cd496fa982b232417d7a0169aa4
DuongVanKhoa0811 <dvkhoa19@apcs.fitus.edu.vn>
2025-12-08 21:51:04 +0800

    Eval PROB_MOWODB_V10 without objectness probability

--- run_eval.sh
+++ run_eval.sh
@@ -2,4 +2,4 @@
 set -ex
 # export CUDA_VISIBLE_DEVICES=2
 
-GPUS_PER_NODE=2 ./tools/run_dist_launch_eval.sh 2 configs/EVAL_M_OWOD_BENCHMARK.sh > ./logs/logs_PROB_MOWODB_EVAL_V10.txt 2>&1
+GPUS_PER_NODE=2 ./tools/run_dist_launch_eval.sh 2 configs/EVAL_M_OWOD_BENCHMARK.sh > ./logs/logs_PROB_MOWODB_EVAL_V10_without_obj_prob.txt 2>&1
