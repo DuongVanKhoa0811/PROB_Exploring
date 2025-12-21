c61f790a3fd7b98a5ca4f5213ecaa1c59ca6be1a
DuongVanKhoa0811 <dvkhoa19@apcs.fitus.edu.vn>
2025-12-22 00:11:20 +0800

    exp_obj_train_only: Test #6 - Eval 18_1

--- run_pbs.sh
+++ run_pbs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #PBS -N PROB_M_V18
 #PBS -q gold
-#PBS -l select=1:ncpus=64:ngpus=8:mem=256gb
+#PBS -l select=1:ncpus=32:ngpus=8:mem=256gb
 #PBS -l walltime=99:00:00
+#PBS -p 00046
 #PBS -j oe
 #PBS -o /home/users/vankhoa_duong/projects/OOD_OD/PROB_Exploring/logs/output.log
 
